b9f73a5f86d833359fd1497454352c9ebef12edc
xlfjn <xlfjn@users.noreply.github.com>
2016-03-19 23:06:40 +0800

    update settings.py to use postgresql and whitenoise

--- ses_maker/wsgi.py
+++ ses_maker/wsgi.py
@@ -4,13 +4,19 @@ WSGI config for ses_maker project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ses_maker.settings")
 
 application = get_wsgi_application()
+
+# For easier local development
+if 'POSTGRESQL_PASSWORD' in os.environ:
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(application)
