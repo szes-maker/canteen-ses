2e462e15468fd5a58d0b2359052817bffce8054f
xlfjn <xlfjn@users.noreply.github.com>
2016-03-19 23:53:43 +0800

    bind gunicorn to 0.0.0.0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python3 manage.py collectstatic -v 3 --no-input
 python3 manage.py migrate
-gunicorn ses_maker.wsgi --log-file -
+gunicorn ses_maker.wsgi --log-file - -b 0.0.0.0:80 --workers=2
