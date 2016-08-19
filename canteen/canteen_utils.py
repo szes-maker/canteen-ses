szes-maker/canteen-ses

commit ed2877bab89ef86c69912aa94fd7c5e21fab11fb

fix login after the school use MD5 to obfuscate password

canteen/canteen_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import re
 import datetime
+from hashlib import md5
 
 import requests
 from lxml import html
@@ -116,7 +117,7 @@ class Login(object):
 
         login_form = {
             'username': username,
-            'password': password,
+            'password': md5(password.encode('utf-8')).hexdigest(),
             'lt': lt,
             '_eventId': 'submit',
             'submit': '登录'

commit 00df62637a58eb7209642373408fa02578b1103a

fix login status detection caused by ed2877b

canteen/canteen_utils.py
@@ -123,7 +123,7 @@ class Login(object):
             'submit': '登录'
         }
         auth = self.session.s_post(login_post_url, login_form, referrer=LOGIN_URL, logged_in=False)
-        auth_status = '<SCRIPT LANGUAGE="JavaScript">' in auth.text
+        auth_status = '登录成功' in auth.text
 
         if auth_status:
             return auth_status, None