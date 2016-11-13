0d01552b2d2503cffc326dcda4815c24bc5d0e78
xlfjn <xlfjn@users.noreply.github.com>
2016-11-13 15:42:33 +0800

    decrease the verbosity of logging to allow debugging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python3 manage.py collectstatic -v 3 --no-input
 python3 manage.py migrate -v 3
-gunicorn ses_maker.wsgi --log-file - -b :80 --workers=2 --log-level debug
+gunicorn ses_maker.wsgi --log-file - -b :80 --workers=2 --log-level warning
