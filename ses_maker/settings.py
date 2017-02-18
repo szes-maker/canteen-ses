5ec21635f60d0b1722b82a5770fdc604d3f9c2b3
xlfjn <xlfjn@users.noreply.github.com>
2017-02-18 12:57:33 +0800

    try to workaround a unknown bug in Daocloud

--- ses_maker/settings.py
+++ ses_maker/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/daocloud-static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGGING = {
