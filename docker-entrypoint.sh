0732c92958cbf85bf5a47a410ba7a2c6424f5aed
xlfjn <xlfjn@users.noreply.github.com>
2017-02-19 12:46:08 +0800

    clear session storage every restart to reduce database size

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 python3 manage.py collectstatic -v 3 --no-input
 python3 manage.py migrate -v 3
+python3 manage.py clearsessions
 gunicorn ses_maker.wsgi --log-file - -b :80 --workers=2 --log-level info
